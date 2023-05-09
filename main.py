1da846f805222292b6f371b0100b4c0c9795f0a2
Стас Басов <stas.basov@whatever.com>
2023-05-09 23:28:53 +0300

    Registration of docstring

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Размер нанесенного урона."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон '
                 f'противнику равный {5 + randint(3, 5)}')
@@ -14,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Количество блокированного урона."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Применение специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное '
                 f'умение «Выносливость {80 + 25}»')
@@ -35,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Тренировка выбранного героя."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -59,6 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор героя. Основные качества."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -80,7 +87,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,4 +100,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-main()
+print()
